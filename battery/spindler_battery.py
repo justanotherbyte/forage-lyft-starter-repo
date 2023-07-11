cfb52aae28b29da7f2fc4915dd5db3f199c53d0d
justanotherbyte <wishymovies@gmail.com>
2023-07-11 12:38:09 +0100

    Reflect spindler battery upgrade in component and in tests

--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -4,5 +4,5 @@ from . import Battery
 
 class SpindlerBattery(Battery):
     def needs_service(self) -> bool:
-        return (self.current_date - self.last_service_date) >= timedelta(days=365 * 2)
+        return (self.current_date - self.last_service_date) >= timedelta(days=365 * 3)
     
--- test/test_battery.py
+++ test/test_battery.py
@@ -7,10 +7,10 @@ from battery.nubbin_battery import NubbinBattery
 class TestBatteries(unittest.TestCase):
     def test_spindler(self):
         today = datetime.today()
-        two_years_ago = (today - timedelta(days=365 * 2))
+        three_years_ago = (today - timedelta(days=365 * 3))
         one_year_ago = (today - timedelta(days=365))
 
-        battery1 = SpindlerBattery(two_years_ago, today)
+        battery1 = SpindlerBattery(three_years_ago, today)
         battery2 = SpindlerBattery(one_year_ago, today)
 
         self.assertTrue(battery1.needs_service())
